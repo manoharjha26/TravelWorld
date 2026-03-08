12a3a5f3348c5325fb4aee8a6efe95add3cfff0b
Manohar Jha <aryabdwaj26@gmail.com>
2026-03-08 06:51:41 -0500

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,2 @@
-// To run locally use local host, for deployment use backend url
-<<<<<<< HEAD
-export const BASE_URL = "https://travel-world-api.vercel.app/api/v1";
-=======
-export const BASE_URL= 'https://travel-world-api.vercel.app/api/v1';  
->>>>>>> ef3aeed8b88c8a571ad41903f965d0632204cb9f
+
+export const BASE_URL = "https://travel-world-rust.vercel.app/api/v1";
