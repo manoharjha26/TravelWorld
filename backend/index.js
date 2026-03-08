7e89440b1528e4a187d1ca5b527c757e07310ad1
Manohar Jha <aryabdwaj26@gmail.com>
2026-03-08 06:10:27 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,13 +27,23 @@ const corsOptions = {
 };
 
 
+// mongoose.set("strictQuery", false);
+// const connect = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI ,{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('MongoDB Is connected successfully');
+//     } catch (err) {
+//         console.log('MongoDB Connection failed');
+//     }
+// };
+
 mongoose.set("strictQuery", false);
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI ,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Is connected successfully');
     } catch (err) {
         console.log('MongoDB Connection failed');
